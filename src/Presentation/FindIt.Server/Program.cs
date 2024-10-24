78b938e1c21c8400ae70a534fd45967d95790508
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-10-24 15:46:30 +0300

    Project Setup

---
+++ src/Presentation/FindIt.Server/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+
+app.UseStaticFiles();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapControllers();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
